af127980a371fa9ac8e60a42ed3cc34b67d01f03
scavengerhuntmj <50850840+scavengerhuntmj@users.noreply.github.com>
2019-05-20 18:13:41 -0600

    Initial commit

---
+++ precache-manifest.dc30eeb7e6ffa314ad101b3407caf5f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa99cb722093e071d32216f10734da31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4650ad6b02fed2a3c381",
+    "url": "/static/css/main.184f3061.chunk.css"
+  },
+  {
+    "revision": "ff0cbb9f1f72c32897cc",
+    "url": "/static/js/2.0cfdc12a.chunk.js"
+  },
+  {
+    "revision": "4650ad6b02fed2a3c381",
+    "url": "/static/js/main.a42e704a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
